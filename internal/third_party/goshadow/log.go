416ea685df75e745cde1f42b5cd0f5f8f5b5ef22
Elsa <zeyugao@outlook.com>
2022-05-19 21:50:08 +0800

    Remove plugin support Warp logger with verbose check

--- internal/third_party/goshadow/log.go
+++ internal/third_party/goshadow/log.go
@@ -9,21 +9,7 @@ import (
 var logger = log.New(os.Stderr, "", log.Lshortfile|log.LstdFlags)
 
 func logf(f string, v ...interface{}) {
-	logger.Output(2, fmt.Sprintf(f, v...))
-}
-
-type logHelper struct {
-	prefix string
-}
-
-func (l *logHelper) Write(p []byte) (n int, err error) {
 	if Verbose {
-		logger.Printf("%s%s\n", l.prefix, p)
-		return len(p), nil
+		logger.Output(2, fmt.Sprintf(f, v...))
 	}
-	return len(p), nil
-}
-
-func newLogHelper(prefix string) *logHelper {
-	return &logHelper{prefix}
 }
